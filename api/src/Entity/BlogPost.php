6143a0252a920982d2906ee51d9f663ccf80fe67
Presley Lupon <presleylupon@gmail.com>
2019-09-24 17:30:52 +0200

    add some stuf

--- api/src/Entity/BlogPost.php
+++ api/src/Entity/BlogPost.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -22,7 +23,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *          "post"={
  *          "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
  *     }
- * }
+ * },
+ *     denormalizationContext={
+ *      "groups"={"post"}
+ *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
  */
@@ -37,6 +41,7 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({“post"})
      * @Assert\NotBlank()
      */
     private $title;
@@ -50,6 +55,7 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({“post"})
      * @Assert\NotBlank()
      */
     private $content;
